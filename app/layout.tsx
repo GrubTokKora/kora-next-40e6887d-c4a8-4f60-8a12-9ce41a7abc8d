f3d422fa26654661b956247afe1d3d159da19351
Kora <noreply@kora.app>
2026-05-01 17:39:21 +0530

    Deploy production from Kora

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,6 +60,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+{/* KORA_TRACKING_GTM_NEXT_START */}
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WBNZJP9H');`,
+        }}
+      />
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-WBNZJP9H"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        />
+      </noscript>
+      {/* KORA_TRACKING_GTM_NEXT_END */}
         <script
           id="kora-jsonld"
           type="application/ld+json"
